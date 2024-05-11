22cdc4f4d1d9be8c9378e77c663db86e0df388d4
岩浆 <linboyan5438081@163.com>
2024-05-12 01:01:54 +0800

    feat: 寻找主人

---
+++ findMaster.js
@@ -0,0 +1,37 @@
+import { plugin, segment, Cfg } from '#Karin'
+export class find_Master extends plugin {
+  constructor () {
+    super({
+      name: '找主人',
+      dsc: '帮走丢的机器人找到回家的路',
+      event: 'message',
+      priority: 10,
+      rule: [
+        {
+          reg: '^#?你主人是谁$',
+          fnc: 'like'
+        }
+      ]
+    })
+  }
+
+  async like () {
+    const msg = []
+    msg.push(segment.text('我的主人是'))
+    for (let master of Cfg.master) {
+      if (typeof master == 'number') {
+        msg.push(segment.text('\n'))
+        msg.push(segment.at(+master))
+        msg.push(segment.text(`(${master})`))
+      }
+    }
+    for (let master of Cfg.admin) {
+      if (typeof master == 'number') {
+        msg.push(segment.text('\n'))
+        msg.push(segment.at(+master))
+        msg.push(segment.text(`(${master})`))
+      }
+    }
+    await this.reply(msg)
+  }
+}
