8bf0c99a8e18f8d0520ab99265dd0efefe7a4270
岩浆 <linboyan5438081@163.com>
2024-11-17 21:39:52 +0800

    fix: [随机老婆]伪支持qqbot

--- 随机老婆.js
+++ 随机老婆.js
@@ -1,4 +1,4 @@
-import { segment, plugin } from 'node-karin'
+import { segment, plugin, redis } from 'node-karin'
 
 export class RandomWife extends plugin {
   constructor () {
@@ -10,20 +10,40 @@ export class RandomWife extends plugin {
       rule: [
         {
           reg: /^#?随机老婆$/,
-          fnc: 'randomWife'
-        }
-      ]
+          fnc: 'randomWife',
+        },
+      ],
     })
   }
 
   async randomWife (e) {
+    if (e.bot.adapter.id === 'QQBot') return this.randomWife_qqbot(e)
     if (!e.isGroup) return await e.reply('别找了，我就你老婆呀，真笨💕')
     const gml = await e.bot.GetGroupMemberList(e.group_id)
     const m = gml[Math.floor(Math.random() * gml.length)]
     const res = [
       segment.text('你的群CP是'),
-      segment.image(`https://q1.qlogo.cn/g?b=qq&s=0&nk=${m.uin}`),
-      segment.text(`【${m.card || m.nick}】(${m.uin})\n看好她哦，别让她乱跑~`)
+      segment.image(e.bot.getAvatarUrl(m.uin)),
+      segment.text(`【${m.card || m.nick}】(${m.uin})\n看好她哦，别让她乱跑~`),
+    ]
+    await e.reply(res, { at: true })
+  }
+
+  async randomWife_qqbot (e) {
+    const today = (new Date()).getDate()
+    let count = 0
+    const group_key = `random_wife:${e.self_id}:${e.group_id}:`
+    const member = await redis.get(group_key + e.user_id)
+    if (member && member.split(':')[1] === today) {
+      count = Number(member.split(':')[0])
+    }
+    count++
+    await redis.set(group_key + e.user_id, `${count}:${today}`)
+    const gml = await redis.keys(group_key + '*')
+    const m = gml[Math.floor(Math.random() * gml.length)]
+    const res = [
+      segment.text('你的群CP是：\n'),
+      segment.image(e.bot.getAvatarUrl(m.replace(group_key, ''))),
     ]
     await e.reply(res, { at: true })
   }
