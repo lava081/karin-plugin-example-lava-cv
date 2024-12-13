4d656d451bb32016da9b52214eaca886f5d70b8b
岩浆 <linboyan5438081@163.com>
2024-12-13 16:13:12 +0800

    fix: [Cmd]优化官方bot下的体验

--- Cmd.js
+++ Cmd.js
@@ -1,5 +1,6 @@
-import { plugin, segment, logger } from 'node-karin'
+import { plugin, segment, logger, render } from 'node-karin'
 import { exec } from 'child_process'
+import fs from 'fs'
 
 export class CommandRunner extends plugin {
   constructor () {
@@ -16,7 +17,7 @@ export class CommandRunner extends plugin {
         },
         {
           reg: /^#?rjs/,
-          fnc: 'run_js',
+          fnc: 'run_command',
           permission: 'admin',
         },
       ],
@@ -24,31 +25,46 @@ export class CommandRunner extends plugin {
   }
 
   /**
-   * 支持在指令末尾添加 `-path=xxx` 来指定工作目录
+   * 支持在指令末尾添加 `-path=xxx` 来指定工作目录，由于目录可能存在空格，该参数必须放在最后
+   * 支持添加 `-msg=xxx` 来指定命令的编码方式，可选'base64'，默认为文本
+   * 支持添加 `-reply=xxx` 来指定回复内容的编码方式，可选'base64'，默认为文本
    */
   async run_command (e) {
-    const cmd = e.msg.replace(/^#?rc/, '').replace(/-path=(.*)$/, '').trim()
+    let cmd = e.msg.replace(/^#?rc/, '').replace(/^#?rjs/, '').replace(/-path=(.*)$/, '').replace(/-reply=([^\s]*)/, '').trim()
     const path = e.msg.match(/-path=(.*)$/)?.[1]
-    this.reply(segment.text('正在执行：' + cmd))
-    let result = await Cmd(cmd, path)
+    const reply_type = e.msg.match(/-reply=([^\s]*)/)?.[1]
+    if (cmd.includes('-msg=base64')) {
+      cmd = (Buffer.from(cmd.replace(/-msg=base64/, '').trim(), 'base64')).toString()
+      this.reply(segment.text('解码成功，正在执行'))
+    } else {
+      this.reply(segment.text('正在执行：' + cmd))
+    }
+    let result = /^#?rc/.test(e.msg) ? await Cmd(cmd, path) : await Js(cmd, e)
     if (typeof result === 'object') {
       result = JSON.stringify(result, null, 2)
     } else if (String(result) === '') {
       result = 'null'
     }
-    return this.reply(segment.text(String(result).trim()))
-  }
-
-  async run_js (e) {
-    const js = e.msg.replace(/^#?rjs/, '').trim()
-    this.reply(segment.text('正在执行js：' + js))
-    let result = await Js(js, e)
-    if (typeof result === 'object') {
-      result = JSON.stringify(result, null, 2)
-    } else if (String(result) === '') {
-      result = 'null'
+    switch (reply_type) {
+      case 'base64': {
+        this.reply(segment.text(Buffer.from(String(result).trim(), 'utf-8').toString('base64')))
+        break
+      }
+      case 'image': {
+        const img = await render.render({
+          file: './resources/karin-plugin-example/Cmd.html',
+          data: {
+            result: String(result).trim(),
+          },
+        })
+        this.reply(segment.image(img))
+        break
+      }
+      default: {
+        this.reply(segment.text(String(result).trim()))
+        break
+      }
     }
-    return this.reply(segment.text(String(result).trim()))
   }
 }
 
@@ -82,4 +98,30 @@ async function Js (cmd, e) {
     logger.warn(error)
     return error.message || error
   }
+}
+
+/**
+ * HTML模板
+ */
+const html = `<!DOCTYPE html>
+<head>
+  <meta charset="UTF-8">
+  <style>
+    .output {
+      white-space: pre-wrap; /* 保留空格和换行 */
+    }
+  </style>
+</head>
+<body>
+  <div class="output">{{result}}</div>
+</body>
+</html>
+`
+
+if (!fs.existsSync('./resources/karin-plugin-example/Cmd.html')) {
+  fs.writeFile('./resources/karin-plugin-example/Cmd.html', html, (err) => {
+    if (err) {
+      logger.error(err)
+    }
+  })
 }
